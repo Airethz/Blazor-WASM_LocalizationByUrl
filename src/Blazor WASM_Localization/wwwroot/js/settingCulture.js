c50b01bc1a53d4da27593b146408d67c138932e7
hongying wu <zxc20012312@yahoo.com.tw>
2022-09-30 21:23:52 +0800

    fix: 調整route放語言參數方式

--- src/Blazor WASM_Localization/wwwroot/js/settingCulture.js
+++ src/Blazor WASM_Localization/wwwroot/js/settingCulture.js
@@ -1,9 +1,9 @@
-﻿window.onload = function () {
+﻿/* loading時檢查lang參數內容 */
+window.onload = function () {
     var langValue = getParameterByName("lang");
     if (langValue.length > 0 && window.localStorage['BlazorCulture'] != langValue) {
         window.localStorage['BlazorCulture'] = langValue;
     }
-
 }
 
 function getParameterByName(name, url = window.location.href) {
@@ -19,3 +19,13 @@ window.blazorCulture = {
     get: () => window.localStorage['BlazorCulture'],
     set: (value) => window.localStorage['BlazorCulture'] = value
 };
+
+/*取得組合url用的lang字串*/
+function GetLangString() {
+    var langVaule = window.localStorage['BlazorCulture'];
+    var result = "";
+    if (langVaule.length > 0) {
+        result = "lang=" + langVaule;
+    }
+    return result;
+}
